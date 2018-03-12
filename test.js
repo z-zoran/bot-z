5c2130d9d9594d94ceb1d69ef9d499bdcd0dfb02
z-oranice <djuricic.zoran@gmail.com>
2018-03-12 18:36:49 +0100

    portfolio wip

--- test.js
+++ test.js
@@ -1,3 +1,24 @@
+
+let memorija = {};
+console.log(memorija);
+memorija.limiti = {};
+console.log(memorija.limiti);
+memorija.limiti.buy = {};
+console.log(memorija.limiti.buy);
+memorija.limiti.sell = {};
+memorija.limiti.buy.iznos = 13;
+console.log(memorija.limiti.buy.iznos);
+
+
+
+let koja = 'buy';
+let a = memorija.limiti[koja].iznos;
+
+console.log(a);
+
+
+
+/*
 let fs = require('fs');
 let pisalo = fs.createWriteStream('./testwritestream.txt');
 
@@ -17,3 +38,4 @@ for (let i = 0; i < 200; i++) {
 }
 
 pisalo.end();
+*/
--- zPortfolio.js
+++ zPortfolio.js
@@ -1,24 +1,22 @@
 "use strict";
 
 // Library s klasama. WIP
-// portfolio pozicija je ustvari portfolio u koji vraćamo sve slobodne pozicije.
+// portfolio je ustvari "glavna pozicija" u koju vraćamo sve slobodne pozicije. 
+// Vezana za strategiju. Svaka strategija imati će svoj portfolio.
 
-let pozzy = require('./zPozzy.js');
 let memorija = require('./zMemy.js');
 let pisalo = require('./zLoggy.js');
 
 /*
 Neka osnovna logika je slijedeća:
 - Portfolio postavlja/ubija limitOrdere.
-	(njihova vrijednost se insta oduzima/dodaje od nultoj poziciji)
+	(njihova vrijednost se insta oduzima/dodaje portfoliju)
 	- LimitOrderi kod triggeranja se samo-ubijaju i stvaraju Poziciju i StopTrigger
 		- StopTrigger se kod triggeranja samo-ubija i stvara Trailer.
-			- Trailer se kod triggeranja samo-ubija, ubija i Poziciju i hrani njen iznos natrag u nultu poziciju.
+			- Trailer se kod triggeranja samo-ubija, ubija i Poziciju i hrani njen iznos natrag u Portfolio.
 
 
 */
-
-
 
 // KLASA ZA PORTFOLIO
 function Portfolio(eur, eth, btc, ltc, bch) {
@@ -32,6 +30,131 @@ function Portfolio(eur, eth, btc, ltc, bch) {
 	this.stopCounter = 0;
 	this.trailerCounter = 0;
 }
+
+
+// METODA ZA POSTAVLJANJE LIMITA
+Portfolio.prototype.postLimit = function postLimit(limitData) {
+	memorija.limiti[limitData.tip] = {};	// iniciramo novi ili brišemo stari limit
+	this.limitCounter += 1; 	// povečavamo counter za limit id
+	let limitCounterString = (this.limitCounter.toString()).padStart(6, "0");
+	let baseTiker = limitData.market.split('/')[0];
+	let quoteTiker = limitData.market.split('/')[1];
+	let umnozak = limitData.iznos * limitData.limitCijena;
+	let poruka = '';
+	if (limitData.tip === 'buy') {
+		// provjera imamo li dovoljno quote u NultojPoziciji za kupiti base
+		if (this[quoteTiker] < umnozak) {
+			poruka = 'BANKROT! Nema dovoljno quote-a za postavljanje buy limita. LimitID: ' + limitCounterString;
+		} else {
+			// exchange komunikacija
+			this[quoteTiker] -= umnozak;
+			memorija.limiti.buy = new limitOrder(limitCounterString, limitData);
+			poruka = 'BUY LIMIT POSTAVLJEN. LimitID: ' + limitCounterString + ', iznos: ' + limitData.iznos + ', limit cijena: ' + limitData.limitCijena;
+		}
+	} else if (limitData.tip === 'sell') {
+		// provjera imamo li dovoljno base u NultojPoziciji za prodaju 
+		if (this[baseTiker] < iznos) {
+			poruka = 'BANKROT! Nema dovoljno base-a za postavljanje sell limita. LimitID: ' + limitCounterString;
+		} else {
+			// exchange komunikacija
+			this[baseTiker] -= limitData.iznos;
+			memorija.limiti.sell = new limitOrder(limitCounterString, limitData);
+			poruka = 'SELL LIMIT POSTAVLJEN. LimitID: ' + limitCounterString + ', iznos: ' + limitData.iznos + ', limit cijena: ' + limitData.limitCijena;
+		}
+	} else {
+		poruka = 'ERROR! limitData nije dobro formatiran. LimitID: ' + limitCounterString;
+	}
+	pisalo.pisi(poruka);
+}
+
+// METODA ZA UBIJANJE LIMITA
+Portfolio.prototype.ubiLimit = function ubiLimit(koji) {
+    // exchange komunikacija
+	let poruka = '';
+	if (koji === 'buy') {
+		this[quote] += memorija.limiti.buy.umnozak;
+		delete memorija.limiti.buy;
+		poruka = 'BUY LIMIT UBIJEN.';
+	} else if (koji === 'sell') {
+		this[base] += memorija.limiti.sell.iznos;
+		delete memorija.limiti.sell;
+		poruka = 'SELL LIMIT UBIJEN.'
+	}
+	pisalo.pisi(poruka);
+}
+
+// METODA ZA REALIZACIJU LIMIT ORDERA, ODNOSNO ZA STVARANJE POZICIJE
+Portfolio.prototype.postPoziciju = function postPoziciju(koja) {
+	let pozData = {};
+	// definiramo pozData za stvaranje nove pozicije.
+	pozData.tip = koja;
+	pozData.market = memorija.limiti[koja].market;
+	pozData.base = memorija.limiti[koja].iznos;
+	pozData.quote = memorija.limiti[koja].umnozak;
+	
+	this.pozCounter += 1; 	// povečavamo counter za id pozicije
+	let pozCounterString = (this.pozCounter.toString()).padStart(6, "0");
+	let novaPoz = new Pozicija(pozCounterString, pozData);
+	memorija.pozicije[pozCounterString] = novaPoz;
+	delete memorija.limiti[koja];
+}
+
+// KLASA ZA LIMIT ORDERE
+function LimitOrder(id, limitData) {
+	this.id = id;	
+	this.tip = limitData.tip;
+	this.market = limitData.market;
+	this.base = limitData.market.split('/')[0];
+	this.quote = limitData.market.split('/')[1];
+	this.iznos = limitData.iznos;
+	this.limitCijena = limitData.limitCijena;
+	this.umnozak = limitData.iznos * limitData.limitCijena;
+}
+
+
+// KLASA ZA POZICIJE
+function Pozicija(id, pozData) {
+	this.id = id;	
+	this.tip = pozData.tip;	// buy || sell
+	this.market = pozData.market;
+	this.base = pozData.base;
+	this.quote = pozData.quote;
+	this.cijena = 
+}
+
+// KLASA ZA STOP TRIGGERE
+function StopTrigger(id, stopData) {
+	this.id = id;
+	this.tip = stopData.tip; // iznad || ispod
+	
+}
+
+// KLASA ZA TRAILERE
+function Trailer(id, trailerData) {
+
+}
+
+/*
+PODACI ZA STOP TRIGGERE
+stopData = {
+	pozicija: idPozicije,
+	tip: 'buy', (ili 'sell')
+	market: 'ETH/EUR',
+	base: 2.345, 
+	quote: 756.78  
+}
+*/
+
+/*
+PODACI ZA POZICIJE
+pozData = {
+	tip: 'buy', (ili 'sell')
+	market: 'ETH/EUR',
+	base: 2.345, 
+	quote: 756.78  
+}
+*/
+
 /*
 PODACI ZA LIMIT ORDERE
 limitData = {
@@ -41,90 +164,3 @@ limitData = {
 	limitCijena: 756.78
 }
 */
-
-// KLASA ZA LIMIT ORDERE
-function LimitOrder(id, limitData) {
-	this.id = id;	
-	this.tip = limitData.tip;
-	this.market = limitData.market;
-	this.iznos = limitData.iznos;
-	this.limitCijena = limitData.limitCijena;
-	this.umnozak = limitData.iznos * limitData.limitCijena;
-}
-
-// METODA ZA POSTAVLJANJE LIMITA
-Portfolio.prototype.postLimit = function postLimit(limitData) {
-	memorija.limiti[limitData.tip] = {};	// iniciramo novi ili brišemo stari limit
-	this.limitCounter += 1; 	// povečavamo counter za limit id
-	let counterString = (this.limitCounter.toString()).padStart(6, "0");
-	let base = limitData.market.split('/')[0];
-	let quote = limitData.market.split('/')[1];
-	let umnozak = limitData.iznos * limitData.limitCijena;
-	let poruka = '';
-	if (limitData.tip === 'buy') {
-		// provjera imamo li dovoljno quote u NultojPoziciji za kupiti base
-		if (this[quote] < umnozak) {
-			poruka = 'BANKROT! Nema dovoljno quote-a za postavljanje buy limita. LimitID: ' + counterString;
-		} else {
-			// exchange komunikacija
-			this[quote] -= umnozak;
-			memorija.limiti.buy = new limitOrder(this.limitCounter, limitData);
-			poruka = 'BUY LIMIT POSTAVLJEN. LimitID: ' + counterString + ', iznos: ' + limitData.iznos + ', limit cijena: ' + limitData.limitCijena;
-		}
-	} else if (limitData.tip === 'sell') {
-		// provjera imamo li dovoljno base u NultojPoziciji za prodaju 
-		if (this[base] < iznos) {
-			poruka = 'BANKROT! Nema dovoljno base-a za postavljanje sell limita. LimitID: ' + counterString;
-		} else {
-			// exchange komunikacija
-			this[base] -= limitData.iznos;
-			memorija.limiti.sell = new limitOrder(this.limitCounter, limitData);
-			poruka = 'SELL LIMIT POSTAVLJEN. LimitID: ' + counterString + ', iznos: ' + limitData.iznos + ', limit cijena: ' + limitData.limitCijena;
-		}
-	} else {
-		poruka = 'ERROR! limitData nije dobro formatiran. LimitID: ' + counterString;
-	}
-	pisalo.pisi(poruka);
-}
-
-// METODA ZA UBIJANJE LIMITA
-Portfolio.prototype.ubiLimit = function ubiLimit(koji) {
-    // exchange komunikacija
-	if (koji === 'buy') {
-		this.quote += memorija.limiti.buy.umnozak;
-	} else if (koji === 'sell') {
-
-	}
-	delete memorija.limiti[koji];
-
-}
-
-/*
-PODACI ZA POZICIJE
-pozData = {
-	tip: 'buy', (ili 'sell')
-	market: 'ETH/EUR',
-	kupljeno: 2.345, (ako je 'buy' onda su ETH, ako je sell onda su 'EUR')
-	prodano: 756.78  (obratno od prodano! valuta ovisi o tome jel buy ili sell...)
-}
-*/
-
-// KLASA ZA POZICIJE
-function Pozicija(id, pozData) {
-	this.id = id;	
-	this.tip = pozData.tip;
-	this.market = pozData.market;
-	this.kupljeno = {};
-	this.prodano = {};
-
-}
-
-// KLASA ZA STOP TRIGGERE
-function StopTrigger(stopData) {
-	
-}
-
-// KLASA ZA TRAILERE
-function Trailer(trailerData) {
-
-}
