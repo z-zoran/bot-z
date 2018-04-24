28eac0a454b9aaf9f6f7cf2e2fe25ff3cd32d77e
z-zoran <djuricic.zoran@gmail.com>
2018-04-25 00:17:23 +0200

    wip

--- test1.js
+++ test1.js
@@ -4,36 +4,28 @@ const fs = require('fs');
 const inputter = fs.createReadStream('exchdata/testdata.csv');
 const potok = agro(inputter, 1, 2, 1);
 
-let br = 3;
-let koliko = 5;
+let br = 2;
 
-function kapaljka(br) {
+potok.on('data', () => {
+    potok.pause();
+})
+
+
+potok.on('readable', () => {
     if (br) {
         console.log(br + ' read: ' + JSON.stringify(potok.read()));
         br--;
     } else {
         potok.pause();
     }
-}
+})
 
-function kapanje(br) {
-    potok.on('readable', () => {
-        if (br) {
-            console.log(br + ' read: ' + JSON.stringify(potok.read()));
-            br--;
-        } else {
-            potok.pause();
-        }
-    })
-}
-kapanje(br);
+
 console.log('Ovo je prvo jer potok još nije readable');
+
+console.log(potok.read());
 
 setTimeout(() => {
     console.log('Ovo je poslje svega');
-
-    br = 0;
+    br = 3;
 }, 1000);
-
-// uspješna logika za kapaljku
-// jebem ti mater potocima i svemu
