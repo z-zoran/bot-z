b2c4f29afa713ea01206debbcb1d0df0d576b12d
z-oranice <djuricic.zoran@gmail.com>
2018-04-26 16:39:20 +0200

    fak je kapaljka radi

--- testing-kapaljka.js
+++ testing-kapaljka.js
@@ -1,33 +1,35 @@
 const agro = require('./agroPotok.js');
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const inputter = fs.createReadStream('exchdata/testdata.csv');
-const potok = agro(inputter, 1, 2, 1);
+const potok = agro(inputter, 15, 2, 1);
 
 let br = 2;
 
-potok.on('data', () => {
-    potok.pause();
-})
+let tekst = '<p>';
 
-let povrat;
+function kapanje(br) {
+    for (let i = 0; i < br; i++) {
+        let jedan = potok.read();
+        if (jedan) {tekst += jedan.datum + ' ' + jedan.sat + ':' + jedan.minuta + ' ' + jedan.C + '<br/>'}
+        else {tekst += 'Nema ništa smisleno <br/>'}
+    }
+}
 
-function kapanje() {
-    let jedan = potok.read();
-    povrat += JSON.stringify(jedan.minuta + ' ' + jedan.C + ' ' + jedan.minuta + '<br/>');
+function parsaj(url) {
+    if (url.slice(0, 4) === '/?i=') {
+        return url.slice(4, url.length);
+    }
 }
 
 // madrfakin server
 http.createServer(function (req, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    if (req.url === '/?i=5') {
-        for (let i = 0; i < 5; i++) {
-            kapanje();
-        }
-    }
     response.write(fs.readFileSync('testing-kapaljka-sajt.html'));
-    response.write(povrat);
+    parsaj(req.url) ? kapanje(parsaj(req.url)) : null;
+    response.write(tekst);
     response.write('</p></body></html>');
     response.end();
 }).listen(1337, '127.0.0.1');
@@ -36,3 +38,4 @@ console.log('Testiranje kapaljke http://127.0.0.1:1337/ ');
 
 
 // USPJEŠNA KAPALJKA!!!
+// implementirati u main.js
